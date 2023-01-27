fc1cd55ce30b3d9e780a83c8e6e878368f8b6245
Phil Chaplin <pchapl1@gmail.com>
2023-01-27 11:18:38 -0800

    finished sum problem

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 'use strict';
+
+
 /////////////////////////////////////
 /* Problem 1 (this is your demo that we'll solve in class)
 Write a function called sum() that takes in two numbers as arguments and then returns an array where the first element is the sum of those numbers, and the second element is a concatenated string that EXACTLY follows this example and uses the values that were input into the function:
@@ -7,13 +9,26 @@ Write a function called sum() that takes in two numbers as arguments and then re
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
+
 // Write your code here
 function sum(a, b) { //eslint-disable-line
 
+    // iniitalize the array
+    let myArr = []
+
+    // insert the sum of a and b at the beginning of the array
+    myArr[0] = a + b
+
+    // create a new variable using template literal and insert it into the array
+    myArr[1] = `The sum of ${a} and ${b} is ${myArr[0]}.`
+
+    return myArr
 }
 
+
+
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 

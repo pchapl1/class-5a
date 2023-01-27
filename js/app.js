021eb50ad8f10f7f64f748b970b2b473749e24f1
Phil Chaplin <pchapl1@gmail.com>
2023-01-27 11:29:24 -0800

    added multiply function

--- js/app.js
+++ js/app.js
@@ -28,10 +28,18 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
 
+    // initialize the array
+    let myArr = []
+
+    myArr[0] = a * b
+
+    myArr[1] = `The product of ${a} and ${b} is ${myArr[0]}.`
+
+    return myArr
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
